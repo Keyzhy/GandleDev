34b8624cbfebd3a5a9beadfd18df10108bbf4a22
Yannis Boulaid <yannisboulaid@mbp-de-yannis.home>
2025-01-14 21:14:59 +0100

    Bugs d'affichages

--- app/(storefront)/products/[name]/page.tsx
+++ app/(storefront)/products/[name]/page.tsx
@@ -104,15 +104,14 @@ export default async function CategoriesPage({
     const { data, title } = await getData(name);
 
     return (
-        <section className="md:mt-40 md:px-24">
-    <h1 className="text-3xl font-semibold my-5 md:ml-12 lg:ml-24">{title}</h1>
-    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 items-start gap-6 px-4 sm:px-8 lg:px-12">
+        <section className="md:mt-40  container max-w-6xl mx-auto"> {/* Padding-top pour décaler sous la navbar */}
+    <h1 className="text-3xl font-semibold my-5 ">{title}</h1>
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 items-start gap-x-6 gap-y-10 lg:gap-x-12 xl:gap-x-20 py-6 sm:px-24">
         {data.map((item) => (
             <ProductCard item={item} key={item.id} />
         ))}
     </div>
 </section>
-
 
     );
 }
--- app/components/storefront/ProductCard.tsx
+++ app/components/storefront/ProductCard.tsx
@@ -17,49 +17,50 @@ interface iAppProps{
 
 
 
-export function ProductCard({ item }: iAppProps) {
-    return (
-        <div className="rounded-2xl w-full md:w-64 lg:w-72 xl:w-80 h-auto mt-3 relative bg-white shadow-lg">
+export function ProductCard({item}: iAppProps){
+    
+    return(
+        <div className="rounded-2xl w-full max-w-xs sm:w-80 h-auto mt-3 relative bg-white shadow-lg ">
+
+            
             <Carousel className="w-full mx-auto">
                 <CarouselContent>
-                    {item.images.map((img, index) => (
+                    
+                    {item.images.map((item, index) =>(
                         <CarouselItem key={index}>
-                            <div className="relative aspect-w-1 aspect-h-1">
-                                <Image
-                                    src={img}
-                                    alt="Product Image"
-                                    fill
-                                    className="object-cover object-center w-full h-full rounded-t-2xl"
-                                />
+                            <div className="relative h-[380px]">
+                                <Image src={item} alt="Product Image" fill className="object-cover object-center w-full h-full rounded-t-2xl"/>
                             </div>
                         </CarouselItem>
                     ))}
                 </CarouselContent>
-                <CarouselPrevious className="ml-16" />
-                <CarouselNext className="mr-16" />
+                
+                <CarouselPrevious className="ml-16"/>
+                <CarouselNext className="mr-16"/>
             </Carousel>
             <Link href={`/product/${item.id}`}>
-                <div className="flex justify-between items-center mt-2">
-                    <h1 className="font-semibold text-xl pl-2">{item.name}</h1>
-                    <h3 className="inline-flex items-center rounded-md bg-[]/10 px-2 py-1 text-s font-medium text-primary ring-1 ring-primary/10">
-                        {item.price} €
-                    </h3>
-                </div>
-                <div className="m-2 flex items-center gap-1">
-                    <StarIcon className="h-4 w-4 text-yellow-500 fill-yellow-500" />
-                    <StarIcon className="h-4 w-4 text-yellow-500 fill-yellow-500" />
-                    <StarIcon className="h-4 w-4 text-yellow-500 fill-yellow-500" />
-                    <StarIcon className="h-4 w-4 text-yellow-500 fill-yellow-500" />
-                    <StarIcon className="h-4 w-4 text-yellow-500" />
-                </div>
-                <span className="absolute top-2 right-2 bg-[#BFA48C] text-white rounded-lg cursor-pointer">
-                    <Plus className="w-10 h-10" />
-                </span>
+            <div className="flex justify-between items-center mt-2">
+            
+                <h1 className="font-semibold text-xl pl-2">{item.name}</h1>
+            
+                <h3 className="inline-flex  items-center rounded-md bg-[]/10 px-2 py-1 text-s font-medium text-primary ring-1 ring-primary/10">{item.price} €</h3>
+            </div>
+            <div className="m-2 flex items-center gap-1">
+            <StarIcon className="h-4 w-4 text-yellow-500 fill-yellow-500" />
+            <StarIcon className="h-4 w-4 text-yellow-500 fill-yellow-500" />
+            <StarIcon className="h-4 w-4 text-yellow-500 fill-yellow-500" />
+            <StarIcon className="h-4 w-4 text-yellow-500 fill-yellow-500" />
+            <StarIcon className="h-4 w-4 text-yellow-500"/>
+          </div>
+            <Link href={`/product/${item.id}`}>
+            <span className=" absolute top-2 right-2 bg-[#BFA48C] text-white rounded-lg cursor-pointer">
+                <Plus className="w-10 h-10"/>
+            </span>
+            </Link>
             </Link>
         </div>
     );
 }
-
 
 export function LoadingProductCard(){
     return(
